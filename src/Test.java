12cbf6c3c10e061baf35821f392ff2092c712f19
AYUSH-BINDAL1 <bindal@purdue.edu>
2023-11-25 00:12:40 -0600

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+/**
+ * Program Name
+ * <p>
+ * Program Description
+ *
+ * @author Ayush Bindal, Lab #
+ * @version 01/01/2023
+ * <p>
+ * Sources: [TA NAMES]
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
