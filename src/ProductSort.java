7961c14d90c882bee8ae7514be2aea12f47637b7
Lionel <lool@purdue.edu>
2023-12-02 23:40:33 -0500

    created all sorts (price and quantity from min to max and max to min) for server side

--- src/ProductSort.java
+++ src/ProductSort.java
@@ -19,15 +19,147 @@ import java.util.Comparator;
 public class ProductSort {
 
     //TODO: Create sortByIncreasingPriceClient method
-    //TODO: Create sortByIncreasingPriceServer method
+    //Sorts all products by min to max price
+    public static ArrayList<String> sortByIncreasingPriceServer(Object LOCK) {
+        ArrayList<String> allProducts = new ArrayList<>();
+        try {
+        synchronized (LOCK) {
+            //creates arraylist of all products
+            allProducts = (ArrayList<String>) Files.readAllLines(Paths.get("Products.txt"));
+        }
+            // sorts all products by min to max with selection sort
+            for (int i = 0; i < allProducts.size() - 1; i++) {
+                int min = i;
+                for(int j = i + 1; j < allProducts.size(); j++) {
+                    String[] productMin = allProducts.get(min).split(",");
+                    double minPrice  = Double.parseDouble(productMin[4]);
+
+                    String[] productCurrent = allProducts.get(j).split(",");
+                    double currentPrice = Double.parseDouble(productCurrent[4]);
+
+                    if (currentPrice < minPrice) {
+                        min = j;
+                    }
+                }
+                if (min != i) {
+                    String temp = allProducts.get(i);
+                    allProducts.set(i, allProducts.get(min));
+                    allProducts.set(min, temp);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return allProducts; //returns product arraylist
+    }
 
     //TODO: Create sortByDecreasingPriceClient method
-    //TODO: Create sortByDecreasingPriceServer method
+    //sorts all products by max to min price
+    public static ArrayList<String> sortByDecreasingPriceServer(Object LOCK) {
+        ArrayList<String> allProducts = new ArrayList<>();
+        try {
+            synchronized (LOCK) {
+                //creates arraylist of all products
+                allProducts = (ArrayList<String>) Files.readAllLines(Paths.get("Products.txt"));
+            }
+            //sorts all products by max to min price by selection sort
+            for (int i = 0; i < allProducts.size() - 1; i++) {
+                int max = i;
+                for(int j = i + 1; j < allProducts.size(); j++) {
+                    String[] productMax = allProducts.get(max).split(",");
+                    double maxPrice  = Double.parseDouble(productMax[4]);
 
-    //TODO: Create sortByIncreasingQuantityServer method
+                    String[] productCurrent = allProducts.get(j).split(",");
+                    double currentPrice = Double.parseDouble(productCurrent[4]);
+
+                    if (currentPrice > maxPrice) {
+                        max = j;
+                    }
+                }
+                if (max != i) {
+                    String temp = allProducts.get(i);
+                    allProducts.set(i, allProducts.get(max));
+                    allProducts.set(max, temp);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return allProducts; //returns product arraylist
+    }
+
     //TODO: Create sortByIncreasingQuantityClient method
+    //sorts all products by min to max quantity
+    public static ArrayList<String> sortByIncreasingQuantityServer(Object LOCK) {
+        ArrayList<String> allProducts = new ArrayList<>();
+        try {
+            synchronized (LOCK) {
+                //creates arraylist of all products
+                allProducts = (ArrayList<String>) Files.readAllLines(Paths.get("Products.txt"));
+            }
+            //sorts all products by min to max quantity by selection sort
+            for (int i = 0; i < allProducts.size() - 1; i++) {
+                int min = i;
+                for(int j = i + 1; j < allProducts.size(); j++) {
+                    String[] productMin = allProducts.get(min).split(",");
+                    double minQuantity  = Double.parseDouble(productMin[5]);
 
-    //TODO: Create sortByDecreasingQuantityServer method
+                    String[] productCurrent = allProducts.get(j).split(",");
+                    double currentQuantity = Double.parseDouble(productCurrent[5]);
+
+                    if (currentQuantity < minQuantity) {
+                        min = j;
+                    }
+                }
+                if (min != i) {
+                    String temp = allProducts.get(i);
+                    allProducts.set(i, allProducts.get(min));
+                    allProducts.set(min, temp);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return allProducts; //returns product arraylist
+    }
+
     //TODO: Create sortByDecreasingQuantityClient method
+    //sorts all products by max to min quantity
+    public static ArrayList<String> sortByDecreasingQuantityServer(Object LOCK) {
+        ArrayList<String> allProducts = new ArrayList<>();
+        try {
+            synchronized (LOCK) {
+                //creates arraylist of all products
+                allProducts = (ArrayList<String>) Files.readAllLines(Paths.get("Products.txt"));
+            }
+            //sorts all products by max to min quantity by selection sort
+            for (int i = 0; i < allProducts.size() - 1; i++) {
+                int max = i;
+                for(int j = i + 1; j < allProducts.size(); j++) {
+                    String[] productMax = allProducts.get(max).split(",");
+                    double maxQuantity = Double.parseDouble(productMax[5]);
+
+                    String[] productCurrent = allProducts.get(j).split(",");
+                    double currentQuantity = Double.parseDouble(productCurrent[5]);
+
+                    if (currentQuantity > maxQuantity) {
+                        max = j;
+                    }
+                }
+                if (max != i) {
+                    String temp = allProducts.get(i);
+                    allProducts.set(i, allProducts.get(max));
+                    allProducts.set(max, temp);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return allProducts; //returns product arraylist
+    }
 
 }
+
+
