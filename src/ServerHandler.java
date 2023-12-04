155294d012eff11121315ae531f8e435bdfdb2bf
AYUSH-BINDAL1 <bindal@purdue.edu>
2023-12-03 22:43:01 -0500

    Finished run method.

--- src/ServerHandler.java
+++ src/ServerHandler.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.io.*;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 
 /**
  * ClientHandler
@@ -17,7 +18,8 @@ public class ServerHandler implements Runnable {
 
     private Socket clientSocket; //Individual Client Socket
     BufferedReader reader; //Reader that reads from the client socket to the server
-    BufferedWriter writer; //Writer that writes from the server to the client socket
+    BufferedWriter writer; //Writer that writes String results to client socket
+    ObjectOutputStream outputStream;  //Output Stream to write an ArrayList to the client socket
     private static Object USERINFOLOCK; //LOCK for synchronizing Username.txt
     private static Object SHOPPINGCARTLOCK; //LOCK for synchronizing ShoppingCart.txt
     private static Object PURCHASEHISTORYLOCK; //LOCK for synchronizing PurchaseHistory.txt
@@ -36,6 +38,7 @@ public class ServerHandler implements Runnable {
         try {
             reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); //Creates reader
             writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream())); //Creates writer
+            outputStream = new ObjectOutputStream(clientSocket.getOutputStream()); //Creates output stream
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -48,7 +51,10 @@ public class ServerHandler implements Runnable {
         String[] commandSplit;
         String command;
         String result;
+        ArrayList<String> resultList = new ArrayList<>();
         do {
+            result = "";
+            resultList.clear();
             try {
                 clientMessage = reader.readLine();
             } catch (IOException e) {
@@ -71,56 +77,77 @@ public class ServerHandler implements Runnable {
                 result = AccountManager.deleteAccount(commandSplit[1], commandSplit[2], USERINFOLOCK,
                         SHOPPINGCARTLOCK, PRODUCTLOCK);
             } else if (command.equalsIgnoreCase("BUY PRODUCT")) {
-
-            }  else if (command.equalsIgnoreCase("CHECKOUT CART")) {
-
+                result =  CustomerShopping.buyProductServer(commandSplit[1], commandSplit[2], PURCHASEHISTORYLOCK,
+                        PRODUCTLOCK);
+            } else if (command.equalsIgnoreCase("CHECKOUT CART")) {
+                result = CustomerShopping.checkoutCartServer(commandSplit[1], SHOPPINGCARTLOCK, PURCHASEHISTORYLOCK);
             } else if (command.equalsIgnoreCase("ADD PRODUCT TO CART")) {
-
+                result = CustomerShopping.addToCartServer(commandSplit[1], commandSplit[2], SHOPPINGCARTLOCK, PRODUCTLOCK);
             } else if (command.equalsIgnoreCase("REMOVE PRODUCT FROM CART")) {
-
+                result = CustomerShopping.removeProductServer(commandSplit[1], commandSplit[2], SHOPPINGCARTLOCK, PRODUCTLOCK);
             } else if (command.equalsIgnoreCase("SEARCH BY NAME")) {
-
+                resultList = ProductSearch.searchByNameServer(commandSplit[1], PRODUCTLOCK);
             } else if (command.equalsIgnoreCase("SEARCH BY STORE")) {
-
+                resultList = ProductSearch.searchByNameServer(commandSplit[1], PRODUCTLOCK);
             } else if (command.equalsIgnoreCase("SEARCH BY DESCRIPTION")) {
-
+                resultList = ProductSearch.searchByNameServer(commandSplit[1], PRODUCTLOCK);
             } else if (command.equalsIgnoreCase("SORT INCREASING PRICE")) {
-
+                resultList = ProductSort.sortByIncreasingPriceServer(PRODUCTLOCK);
             } else if (command.equalsIgnoreCase("SORT DECREASING PRICE")) {
-
+                resultList = ProductSort.sortByDecreasingPriceServer(PRODUCTLOCK);
             } else if (command.equalsIgnoreCase("SORT INCREASING QUANTITY")) {
-
+                resultList = ProductSort.sortByIncreasingQuantityServer(PRODUCTLOCK);
             } else if (command.equalsIgnoreCase("SORT DECREASING QUANTITY")) {
-
+                resultList = ProductSort.sortByDecreasingQuantityServer(PRODUCTLOCK);
             } else if (command.equalsIgnoreCase("VIEW PURCHASE HISTORY")) {
-
+                resultList = PurchaseHistory.viewCustomerPurchaseHistoryServer(commandSplit[1], PURCHASEHISTORYLOCK);
             } else if (command.equalsIgnoreCase("EXPORT PURCHASE HISTORY")) {
-
-            }  else if (command.equalsIgnoreCase("VIEW CUSTOMER STATISTICS")) {
-
-            }  else if (command.equalsIgnoreCase("VIEW SELLER PRODUCTS")) {
-
-            }  else if (command.equalsIgnoreCase("MODIFY PRODUCT")) {
-
-            }  else if (command.equalsIgnoreCase("DELETE PRODUCT")) {
-
-            }  else if (command.equalsIgnoreCase("CREATE NEW PRODUCT")) {
-
-            }  else if (command.equalsIgnoreCase("VIEW SALES BY STORE")) {
-
-            }  else if (command.equalsIgnoreCase("IMPORT CSV")) {
-
-            }  else if (command.equalsIgnoreCase("EXPORT CSV")) {
-
-            }  else if (command.equalsIgnoreCase("VIEW SELLER STATISTICS")) {
-
-            }  else if (command.equalsIgnoreCase("VIEW SELLER SHOPPING CART")) {
-
+                result = PurchaseHistory.exportCustomerPurchaseHistoryServer(commandSplit[1], commandSplit[2],
+                        PURCHASEHISTORYLOCK);
+            } else if (command.equalsIgnoreCase("VIEW CUSTOMER STATISTICS")) {
+                //TODO: IS THIS DONE???
+            } else if (command.equalsIgnoreCase("VIEW SELLER PRODUCTS")) {
+                resultList = SellerShopping.viewSellerProducts(commandSplit[1], PRODUCTLOCK);
+            } else if (command.equalsIgnoreCase("MODIFY PRODUCT")) {
+                result = SellerShopping.modifyProductServer(commandSplit[1], commandSplit[2],
+                        commandSplit[3], commandSplit[4], Double.parseDouble(commandSplit[5]),
+                        Integer.parseInt(commandSplit[6]), commandSplit[7], SHOPPINGCARTLOCK, PRODUCTLOCK);
+            } else if (command.equalsIgnoreCase("DELETE PRODUCT")) {
+                result = SellerShopping.deleteProductServer(commandSplit[1], SHOPPINGCARTLOCK, PRODUCTLOCK);
+            } else if (command.equalsIgnoreCase("CREATE NEW PRODUCT")) {
+                result = SellerShopping.createNewProductServer(commandSplit[1], commandSplit[2],
+                        commandSplit[3], commandSplit[4], Double.parseDouble(commandSplit[5]),
+                        Integer.parseInt(commandSplit[6]), PRODUCTLOCK);
+            } else if (command.equalsIgnoreCase("VIEW SALES BY STORE")) {
+                result = Statistics.viewSalesByStoreServer(commandSplit[1], PURCHASEHISTORYLOCK);
+            } else if (command.equalsIgnoreCase("IMPORT CSV")) {
+                result = CSVHandler.importSellerCSVServer(commandSplit[1], PRODUCTLOCK);
+            } else if (command.equalsIgnoreCase("EXPORT CSV")) {
+                result = CSVHandler.exportSellerCSVServer(commandSplit[1], commandSplit[2], PRODUCTLOCK);
+            } else if (command.equalsIgnoreCase("VIEW SELLER STATISTICS")) {
+                result = Statistics.generateSellerDashboardServer(commandSplit[1], Integer.parseInt(commandSplit[2])
+                        , PURCHASEHISTORYLOCK);
+            } else if (command.equalsIgnoreCase("VIEW SELLER SHOPPING CART")) {
+                resultList = SellerShopping.getSellerShoppingCartServer(commandSplit[1], PRODUCTLOCK);
             } else if (command.equalsIgnoreCase("EXIT PROGRAM")) {
                 exitProgram();
             }
-        } while (!command.equalsIgnoreCase("EXIT PROGRAM"));
 
+            if(result.isEmpty()) {
+                try {
+                    outputStream.writeObject(resultList);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else if(resultList.isEmpty()) {
+                try {
+                    writer.write(result);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } while (!command.equalsIgnoreCase("EXIT PROGRAM"));
     }
 
 
