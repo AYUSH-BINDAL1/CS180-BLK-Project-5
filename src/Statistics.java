b3d94ca47ff9d07cc1fd336eaba7eb65d609a691
Lionel <lool@purdue.edu>
2023-12-03 22:36:52 -0500

    fixed statistics customer side

--- src/Statistics.java
+++ src/Statistics.java
@@ -157,7 +157,7 @@ public class Statistics {
     //TODO: Create sortCustomerHightoLowClient method
     //TODO: Create sortCustomerHightoLowServer method
     //sort from high to low
-    public static ArrayList<String> sortQuantityMax(ArrayList<String> toSort) {
+    public static ArrayList<String> sortCustomerHightoLowServer(ArrayList<String> toSort) {
         // selection sort
         for (int i = 0; i < toSort.size() - 1; i++) {
             int max = i;
